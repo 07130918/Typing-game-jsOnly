761db32e6c62acd55b252558b7fce6fe4c28ebaa
kotawindows <kotawonderlandjapan@outlook.jp>
2020-11-24 12:08:08 +0900

    timer

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-import {strs} from "./strs.js"
+import { strs } from "./strs.js"
+// DOM
 const timeElement = document.getElementById('time')
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const reset = document.getElementById('reset')
-
 const typedField = document.getElementById('typed');
 const untypedField = document.getElementById('untyped');
 
 const time = 10000;
+// 経過時間(ミリ秒)
+let elapsed = 0;
+let intervalId = null;
 
-
-let typed = ''
+let typed = '';
 let untyped = 'Start';
+
+// 関数
+function updateTime () {
+  const ms = elapsed % 1000;
+  const s = Math.floor(elapsed / 1000) % 60; //小数切り捨て
+  const mi = Math.floor(elapsed / (1000 * 60)) % 60;
+  const h = Math.floor(elapsed / (1000 * 60 * 60)) % 60; 
+
+  const msStr = ms.toString().padStart(3, '0') //数値を文字列に変換
+  const sStr = s.toString().padStart(2, '0')
+  const miStr = mi.toString().padStart(2, '0')
+  const hStr = h.toString().padStart(2, '0')
+
+  timeElement.innerHTML = `${hStr}:${miStr}:${sStr}.${msStr}`;
+}
 
 function getRandomInt(max) { //単語ランダム抽出
   return Math.floor(Math.random() * Math.floor(max));
@@ -19,7 +36,7 @@ function getRandomInt(max) { //単語ランダム抽出
 
 function nextString() {//ランダムに抽出された単語を返す
   const i = getRandomInt(strs.length);
-  return strs[i]; 
+  return strs[i];
 }
 
 function updateTextField() {
@@ -33,6 +50,30 @@ function next() {
   updateTextField();
 }
 
+// タイマーイベント
+start.addEventListener('click', function (e) {
+  if (intervalId !== null) { return; }
+  let pre = new Date();
+  intervalId = setInterval(function () { //第2引数の時間ごとに第1引数の関数の処理を行う
+    const now = new Date();
+    elapsed += now - pre;
+    pre = now;
+    updateTime();
+    console.log(elapsed);
+  }, 10)
+})
+
+stop.addEventListener('click', function () {
+  clearInterval(intervalId);
+  intervalId = null; //start連打で何回もタイマーが裏で動作しないように
+})
+
+reset.addEventListener('click', function () {
+  elapsed = 0;
+  updateTime();
+})
+
+//タイピングイベント
 document.addEventListener('keydown', function (e) {
   if (e.key !== untyped.substring(0, 1)) { return; } //押したキーが正しくないとき何もせず返す
   typed += untyped.substring(0, 1); //はじまりから1文字とる
