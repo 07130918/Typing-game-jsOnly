e610cdc9b4ecf23f0b3159ce207fcffbff7a4f2f
kotawindows <kotawonderlandjapan@outlook.jp>
2020-11-25 09:52:24 +0900

    Code refactoring

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 import { strs } from "./strs.js"
 // DOM
 const timeElement = document.getElementById('time');
+const shortTime = document.getElementById('shortTime');
+const midiumTime = document.getElementById('midiumTime');
+const longTime = document.getElementById('longTime');
 const start = document.getElementById('start');
-// const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
 const result = document.getElementById('result');
 const typedField = document.getElementById('typed');
 const untypedField = document.getElementById('untyped');
 
+let time;
+//時間指定イベント
+shortTime.addEventListener('click', function () {
+  time = 10000;
+  updateTime();
+
+})
+midiumTime.addEventListener('click', function () {
+  time = 20000;
+  updateTime();
+})
+longTime.addEventListener('click', function () {
+  time = 30000;
+  updateTime();
+})
+
 // 経過時間(ミリ秒)
 let intervalId = null;
-const time = 10000;
-let remaining = time;
-
 let typed = '';
 let untyped = 'Start';
 let missTypeCount = 0;
@@ -20,10 +35,10 @@ let successTypeCount = 0;
 
 // 関数
 function updateTime() {
-  const ms = remaining % 1000;
-  const s = Math.floor(remaining / 1000) % 60; //小数切り捨て
-  const mi = Math.floor(remaining / (1000 * 60)) % 60;
-  const h = Math.floor(remaining / (1000 * 60 * 60)) % 60;
+  const ms = time % 1000;
+  const s = Math.floor(time / 1000) % 60; //小数切り捨て
+  const mi = Math.floor(time / (1000 * 60)) % 60;
+  const h = Math.floor(time / (1000 * 60 * 60)) % 60;
 
   const msStr = ms.toString().padStart(3, '0') //数値を文字列に変換
   const sStr = s.toString().padStart(2, '0')
@@ -33,12 +48,8 @@ function updateTime() {
   timeElement.innerHTML = `${hStr}:${miStr}:${sStr}.${msStr}`;
 }
 
-function getRandomInt(max) { //単語ランダム抽出
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function nextString() {//ランダムに抽出された単語を返す
-  const i = getRandomInt(strs.length);
+function getRandomWord() { //単語ランダム抽出
+  const i = Math.floor(Math.random() * Math.floor(strs.length));
   return strs[i];
 }
 
@@ -49,67 +60,62 @@ function updateTextField() {
 
 function next() { //新たな問題を表示,1から単語をタイピング指せる関数
   typed = '';
-  untyped = nextString();
+  untyped = getRandomWord();
   updateTextField();
 }
 
-function keyDownCallback(e) {
+function keypressCallback(e) {
   if (e.key !== untyped.substring(0, 1)) { //押したキーが正しくないとき
-    missTypeCount += 1;
+    missTypeCount++;
     return;
   }
-  successTypeCount += 1;
+  successTypeCount++;
   typed += untyped.substring(0, 1); //はじまりから1文字とる
   untyped = untyped.substring(1);//2文字目から末尾まで
 
   updateTextField();
 
-  if (untyped === '') { //全部打ち終わったら
-    next();
-  }
+  if (untyped === '') { next() } //全部打ち終わったら
 }
 
 function gameStart() {
   let pre = new Date();
   intervalId = setInterval(function () { //第2引数の時間ごとに第1引数の関数の処理を行う
     const now = new Date();
-    remaining -= now - pre;
-    if (remaining < 0) {
+    time -= now - pre;
+    if (time < 0) {
       gameEnd();
     }
     pre = now;
     updateTime();
   }, 10)
   //タイピングイベント
-  document.addEventListener('keydown', keyDownCallback);
+  document.addEventListener('keypress', keypressCallback);
   next();
 }
 
 function gameEnd() {
-  remaining = 0;
+  time = 0;
   updateTime();
   clearInterval(intervalId);
   intervalId = null;
-  document.removeEventListener('keydown', keyDownCallback);
-  result.textContent = `ミスタイプ数: ${missTypeCount}, 総タイプ数${missTypeCount+successTypeCount}`
+  document.removeEventListener('keypress', keypressCallback);
+  result.textContent = `ミスタイプ数: ${missTypeCount}, 総タイプ数${missTypeCount + successTypeCount}`
+  alert('終了!!!');
 }
 
 // タイマーイベント
 start.addEventListener('click', function () {
+  if (time === 0) { alert('時間を選んでください') }
   if (intervalId !== null) { return; }
   gameStart();
 })
 
-// stop.addEventListener('click', function () {
-//   clearInterval(intervalId); //setIntervalを使用した動作のキャンセル
-//   intervalId = null; //start連打で何回もタイマーが裏で動作しないように
-// })
-
 reset.addEventListener('click', function () {
-  document.removeEventListener('keydown', keyDownCallback);
+  document.removeEventListener('keypress', keypressCallback);
   clearInterval(intervalId);
   intervalId = null;
-  remaining = time;
+  time = 0;
   updateTime();
   next();
   missTypeCount = 0;
